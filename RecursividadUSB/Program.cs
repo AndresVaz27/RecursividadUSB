893b9f1d0cad648eaa493ede779883fc528e151e
AndresVaz27 <andresvazquezo1@hotmail.com>
2023-08-28 19:11:34 -0600

    Add project files.

---
+++ RecursividadUSB/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursividadUSB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la ruta de la memoria USB: ");
+            string usbPath = Console.ReadLine();
+
+            if (Directory.Exists(usbPath))
+            {
+                Console.WriteLine($"Contenido de {usbPath}:");
+                ListarCarpetasYArchivos(usbPath);
+            }
+            else
+            {
+                Console.WriteLine("La ruta especificada no es válida.");
+            }
+            Console.ReadKey();
+        }
+
+        static void ListarCarpetasYArchivos(string path, string indent = "")
+        {
+            try
+            {
+                string[] carpetas = Directory.GetDirectories(path);
+                foreach (string carpeta in carpetas)
+                {
+                    Console.WriteLine($"{indent}Carpeta: {Path.GetFileName(carpeta)}");
+                    ListarCarpetasYArchivos(carpeta, indent + "  ");
+                }
+
+                string[] archivos = Directory.GetFiles(path);
+                foreach (string archivo in archivos)
+                {
+                    Console.WriteLine($"{indent}Archivo: {Path.GetFileName(archivo)}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{indent}Error: {ex.Message}");
+            }
+        }
+    }
+}
